e0c2ddac6fd8d1fd579a023a9fb94a75250baa34
e1b16057 <e1b16057@users.noreply.github.com>
2018-09-26 19:29:51 +0900

    オブジェクトの生成および移動

---
+++ VRrhythmGame/Assets/ballCreateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+
+public class ballCreateScript : MonoBehaviour
+{
+
+    public GameObject cubePrefab;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Random.Range(0, 60) >= 58)
+        {
+            // Instantiateの引数にPrefabを渡すことでインスタンスを生成する
+            GameObject cube = Instantiate(cubePrefab) as GameObject;
+            // ランダムな場所に配置する
+            cube.transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), Random.Range(20, 24));
+
+        }
+
+    }
+}
---
+++ VRrhythmGame/Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(0f, 0f, 6f * Time.deltaTime);
+        if (transform.position.z <= -4)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
