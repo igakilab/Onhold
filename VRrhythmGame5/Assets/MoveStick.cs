81b9c6c54420331d910b84520a38941d21fafc09
murai0823 <40621929+murai0823@users.noreply.github.com>
2018-10-25 15:20:27 +0900

    シーン追加
    スタート画面リザルト画面追加

---
+++ VRrhythmGame5/Assets/MoveStick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStick : MonoBehaviour {
+
+    Boolean b = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x <= 0)
+        {
+            b = true;
+        }
+        if (transform.position.x > 3)
+        {
+            b = false;
+        }
+
+            transform.position -= new Vector3(6f * Time.deltaTime * (b ? -1 : 1), 0, 0);
+    }
+}
